6a16a56a75db36b25a28f5986de848e06a0dae49
Brandur <brandur@brandur.org>
2019-08-11 15:32:26 -0700

    Add another test for `WithInternalError`

--- server/main_test.go
+++ server/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"testing"
 
@@ -13,4 +14,19 @@ func TestAPIError_Error(t *testing.T) {
 		"API error status 400: foo",
 		err.Error(),
 	)
+}
+
+func TestAPIError_WithInternalError(t *testing.T) {
+	apiErr := &APIError{http.StatusBadRequest, "foo", nil}
+	internalErr := fmt.Errorf("internal error")
+
+	dupErr := apiErr.WithInternalError(internalErr)
+
+	// New error should be a duplicate with an internal error.
+	assert.Equal(t, apiErr.StatusCode, dupErr.StatusCode)
+	assert.Equal(t, apiErr.Message, dupErr.Message)
+	assert.Equal(t, internalErr, dupErr.internalErr)
+
+	// The original error should still have a `nil` internal error.
+	assert.Nil(t, apiErr.internalErr)
 }
